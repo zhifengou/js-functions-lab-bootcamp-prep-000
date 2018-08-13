e4f9e9f988037b92e39ea678ebcde0d0e61ea5b8
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-13 16:17:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return "Happy holidays,${name}!"
 }
-happyHolidayTo(' you')
+happyHolidaysTo(' you')
 function happyHolidayTo(holiday,name){
   return "Happy ${holiday},${name}!"
 }
